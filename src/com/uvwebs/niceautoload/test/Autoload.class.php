288607c86920dfb0f7d60a01d1eb4e44d864b77c
Georgi Lambov <dev@uvwebs.com>
2010-08-23 19:59:20 +0300

    Commit dev branch files

--- src/com/uvwebs/niceautoload/test/Autoload.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-class niceautoload_test_Autoload
-{
-	public function __toString()
-	{
-		echo 'Autoload works';
-	}
-}
-
--- src/com/uvwebs/niceautoload/test/TestAutoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'bootstrap.php';
-$i = new niceautoload_test_Autoload();
-var_dump($i);
-
